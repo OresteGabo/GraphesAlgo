8fdaadb6fe86241cacb611987b95bc1900e5d498
oreste <muhirwa.g.oreste@gmail.com>
2021-04-05 00:18:47 +0200

    bug test

--- graphenonorientevalue.cpp
+++ graphenonorientevalue.cpp
@@ -3,6 +3,6 @@
 GrapheNonOrienteValue::GrapheNonOrienteValue(){}
 GrapheNonOrienteValue::~GrapheNonOrienteValue(){}
 bool GrapheNonOrienteValue::connectionExiste(Sommet*,Sommet*)const{return true;}
-void GrapheNonOrienteValue::affiche() const{kk
+void GrapheNonOrienteValue::affiche() const{
     cout<<"GrapheNonOrienteValue::affiche() executed correctly"<<endl;
 }
